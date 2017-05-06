d4c00fd170c21882a03d8b2477490eb70e093099
trit <trit@quantumtrip.org>
2017-05-05 21:47:32 -0400

    Commiting changes

---
+++ ConsoleApplication1/stdafx.cpp
@@ -0,0 +1,38 @@
+﻿// stdafx.ch : source file that includes just the standard includes
+// ConsoleApplication1.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+//qt_stdafx.cc
+//( game = new game() { run.me }).eject(nonsense)->to-....u(super)<mother>[consoled]>>> 
+	//output: any game data that didn't get processed;
+/*	a ticket out of relative poverty
+
+''todo -> make this compile(run)
+
+#include "stdafx.h"
+
+namespace GalacticTrip {
+	class Ganame {
+		stream<utf∞> run(input);
+
+		static double setOptions(preLaunch, postLaunch, postpostLaunch-- - ) {
+
+		}
+}
+*/
+
+#include "stdafx.h"
+namespace QuantumTrip
+{
+	Game::Game()
+	{
+
+	}
+
+	EndState Game::run()
+	{
+		Game g;
+		if(g.start()) {
+
+	}
+};
